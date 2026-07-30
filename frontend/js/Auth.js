c638cb23f58ef239c961a5e01259aee1db252b48
DucGiap <giapduc2299@gmail.com>
2026-07-30 10:10:35 +0700

    fix: cors

--- frontend/js/Auth.js
+++ frontend/js/Auth.js
@@ -43,7 +43,7 @@ async function handleLogin(event) {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                'Accept': 'application/json'
+                // 'Accept': 'application/json'
             },
             body: JSON.stringify({
                 username: usernameInput.value.trim(),
