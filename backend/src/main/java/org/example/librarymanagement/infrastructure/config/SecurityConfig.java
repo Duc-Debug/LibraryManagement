b1c3629ddfae0ba60cfad4e3df76c90f0e5ea5e4
Viet nguyen <nguyenvietbl98@gmail.com>
2026-08-11 14:05:56 +0700

    fix (import)

--- backend/src/main/java/org/example/librarymanagement/infrastructure/config/SecurityConfig.java
+++ backend/src/main/java/org/example/librarymanagement/infrastructure/config/SecurityConfig.java
@@ -22,9 +22,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import jakarta.servlet.http.HttpServletResponse;
+import tools.jackson.databind.ObjectMapper;
 
 @Configuration
 @EnableMethodSecurity
